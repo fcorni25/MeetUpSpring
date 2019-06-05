cec27668c32f9289aab189593147c444c85d63bc
fcorni25 <francoise.cornille@decathlon.com>
2019-06-05 09:18:45 +0200

    message

--- src/main/java/com/decathlon/MeetUpSpring/configuration/Configuration.java
+++ src/main/java/com/decathlon/MeetUpSpring/configuration/Configuration.java
@@ -1,5 +1,14 @@
 package com.decathlon.MeetUpSpring.configuration;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootConfiguration
 public class Configuration {
 
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
 }
--- src/main/java/com/decathlon/MeetUpSpring/controller/EventController.java
+++ src/main/java/com/decathlon/MeetUpSpring/controller/EventController.java
@@ -1,5 +1,6 @@
 package com.decathlon.MeetUpSpring.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.decathlon.MeetUpSpring.dto.EventDTO;
+import com.decathlon.MeetUpSpring.models.Event;
 import com.decathlon.MeetUpSpring.service.EventService;
 
 @RestController
 public class EventController {
 
 	private EventService eventService;
+	private ModelMapper eventMapper;
 	
 	// pour eviter @Autowired
-	public EventController(EventService eventService) {
+	public EventController(EventService eventService, ModelMapper eventMapper) {
 		this.eventService = eventService;
+		this.eventMapper = eventMapper;
 	}
+
 
 	@GetMapping("/ping")
 		public String pong() {
@@ -31,6 +36,19 @@ public class EventController {
 		public EventDTO addNewEvent(@RequestBody EventDTO eventDTO) {
 			return eventService.create(eventDTO);
 		};
+	
 		
+//	@GetMapping("/events")
+//		public
+	
+		
+		private EventDTO convertToEventDto(Event event) {
+		    return eventMapper.map(event, EventDTO.class);
+		  }
+
+		  private Event convertToEvent(EventDTO eventDTO) {
+		    return eventMapper.map(eventDTO, Event.class);
+		  }
+	
 		
 }
